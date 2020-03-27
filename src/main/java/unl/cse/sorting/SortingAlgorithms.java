8ee47888a246b2dba2aa115d87235df3e9d40957
Chris Bourke <cbourke@cse.unl.edu>
2020-03-26 21:37:44 -0500

    formatting, updated handout to reflect junit

--- src/main/java/unl/cse/sorting/SortingAlgorithms.java
+++ src/main/java/unl/cse/sorting/SortingAlgorithms.java
@@ -4,18 +4,18 @@ import java.util.Arrays;
 
 public class SortingAlgorithms {
 
-	public static Location [] javaSort(Location list[]) {
+	public static Location[] javaSort(Location list[]) {
 		Location result[] = Arrays.copyOf(list, list.length);
 		Arrays.sort(result);
 		return result;
 	}
-	
-	public static Location [] selectionSort(Location list[]) {
+
+	public static Location[] selectionSort(Location list[]) {
 		Location result[] = Arrays.copyOf(list, list.length);
-		for(int i=0; i<result.length; i++) {
+		for (int i = 0; i < result.length; i++) {
 			int minIndex = i;
-			for(int j=i+1; j<result.length; j++) {
-				if(result[j].compareTo(result[minIndex]) < 0)
+			for (int j = i + 1; j < result.length; j++) {
+				if (result[j].compareTo(result[minIndex]) < 0)
 					minIndex = j;
 			}
 			Location tmp = result[i];
@@ -25,19 +25,19 @@ public class SortingAlgorithms {
 		return result;
 	}
 
-	public static Location [] insertionSort(Location list[]) {
+	public static Location[] insertionSort(Location list[]) {
 		Location result[] = Arrays.copyOf(list, list.length);
-		//TODO: implement insertion sort here
+		// TODO: implement insertion sort here
 		return result;
 	}
-	
-	public static Location [] quickSort(Location list[]) {
+
+	public static Location[] quickSort(Location list[]) {
 		Location result[] = Arrays.copyOf(list, list.length);
-		quickSortRecursive(result, 0, result.length-1);
+		quickSortRecursive(result, 0, result.length - 1);
 		return result;
 	}
 
 	private static void quickSortRecursive(Location list[], int low, int high) {
-		//TODO: implement quick sort here
+		// TODO: implement quick sort here
 	}
 }
--- src/test/java/unl/cse/sorting/SortingTests.java
+++ src/test/java/unl/cse/sorting/SortingTests.java
@@ -10,60 +10,62 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SortingTests {
-    int locationCount = 500;
-    Location[] locations = new Location[locationCount];
-    @BeforeEach
-    void loadFromFile() {
+	int locationCount = 500;
+	Location[] locations = new Location[locationCount];
 
-        List<Location>temp = new ArrayList<>();
-        try {
-            File file = new File("data/geoData.csv");
-            Scanner scanner = new Scanner(file);
-            //throw away first line
-            scanner.nextLine();
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                //create airport objects
-                String[] arr = line.split(",");
+	@BeforeEach
+	void loadFromFile() {
 
-                Integer z = Integer.parseInt(arr[0]);
-                String zip = String.format("%05d", z);
-                double lat = 0d;
-                double lon = 0d;
-                String city = arr[3] == null ? "" : arr[3];
-                String state = arr[4] == null ? "" : arr[4];
-                try {
-                    lat = Double.parseDouble(arr[1]);
-                    lon = Double.parseDouble(arr[2]);
-                } catch (NumberFormatException nfe) {
-                    //silence exceptions
-                }
-                Location location = new Location(zip, lat, lon, city, state);
-                temp.add(location);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        Collections.shuffle(temp);
-        for (int i = 0; i < locationCount; i++) {
-            locations[i] = temp.get(i);
-        }
-    }
+		List<Location> temp = new ArrayList<>();
+		try {
+			File file = new File("data/geoData.csv");
+			Scanner scanner = new Scanner(file);
+			// throw away first line
+			scanner.nextLine();
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				// create airport objects
+				String[] arr = line.split(",");
 
-    @Test
-    void insertionSortTest() {
-        Location[] sortedExpectation = Arrays.copyOf(locations, locations.length);
-        Arrays.sort(sortedExpectation);
+				Integer z = Integer.parseInt(arr[0]);
+				String zip = String.format("%05d", z);
+				double lat = 0d;
+				double lon = 0d;
+				String city = arr[3] == null ? "" : arr[3];
+				String state = arr[4] == null ? "" : arr[4];
+				try {
+					lat = Double.parseDouble(arr[1]);
+					lon = Double.parseDouble(arr[2]);
+				} catch (NumberFormatException nfe) {
+					// silence exceptions
+				}
+				Location location = new Location(zip, lat, lon, city, state);
+				temp.add(location);
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		Collections.shuffle(temp);
+		for (int i = 0; i < locationCount; i++) {
+			locations[i] = temp.get(i);
+		}
+	}
 
-        assertArrayEquals(sortedExpectation, SortingAlgorithms.insertionSort(sortedExpectation));
-    }
+	@Test
+	void insertionSortTest() {
+		Location[] sortedExpectation = Arrays.copyOf(locations, locations.length);
+		Arrays.sort(sortedExpectation);
 
-    @Test
-    void quickSortTest() {
-        Location[] sortedExpectation = Arrays.copyOf(locations, locations.length);
-        Arrays.sort(sortedExpectation);
+		assertArrayEquals(sortedExpectation, SortingAlgorithms.insertionSort(sortedExpectation));
+	}
 
-        assertArrayEquals(sortedExpectation, SortingAlgorithms.quickSort(sortedExpectation));
-    }
+	@Test
+	void quickSortTest() {
+		Location[] sortedExpectation = Arrays.copyOf(locations, locations.length);
+		Arrays.sort(sortedExpectation);
+
+		assertArrayEquals(sortedExpectation, SortingAlgorithms.quickSort(sortedExpectation));
+	}
 
 }
